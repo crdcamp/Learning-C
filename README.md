90b9f959d3b624174c1dec324a5a44e6d059101c
Christian Camp <crdcamp@gmail.com>
2026-09-22 18:49:06 -0400

    Final commit before pushing to remote

---
+++ README.md
@@ -0,0 +1,1 @@
+Just a curious boy practicing C cause I think memory management is very interesting.
--- practice/1_linked_lists/CSE_333_hw.c
+++
@@ -1,6 +0,0 @@
-// Link: https://courses.cs.washington.edu/courses/cse333/26wi/hw/hw1/
-#include <stdio.h>
-
-int main(void) {
-    return 0;
-}
