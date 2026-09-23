1d066a05560d2df00062a1c7f7e891b4b273fc0e
Christian Camp <crdcamp@gmail.com>
2026-09-22 20:57:12 -0400

    Break time

--- practice/1_linked_lists/w3resource_practice/2_reverse_display.c
+++ practice/1_linked_lists/w3resource_practice/2_reverse_display.c
@@ -3,8 +3,8 @@
 // Write a program to create a singly linked list of n nodes and display it in reverse order.
 
 typedef struct node {
-    int integer;
-    struct node *next;
+    int number;
+    struct node *next_node;
 } node;
 
 int main(void) {
@@ -19,7 +19,32 @@ int main(void) {
             return 1;
         }
 
-        // Assign node with an integer value to allocated memory
+        n->number = i + 1; // Assign node with an integer value to newly allocated node memory
+        n->next_node = NULL; // Safety first (I forget why we even do this)
+        n->next_node = list; // Assign previous node's address so we can backtrack
+        list = n; // Ensure the most recent node is the beginning of the linked list
+    }
 
-    return 0;
+    // Print list in reverse order
+    node *ptr = list;
+    printf("REVERSED ORDER:\n");
+    while (ptr != NULL) {
+        printf("Meow %i\n", ptr->number);
+        ptr = ptr->next_node;
+    }
+
+    // Reset pointer
+    // YOU'RE GONNA WANNA DEFINE SOME MORE THINGS
+    // UP HERE TO BE ABLE TO TRAVERSE BACKWARDS
+    ptr = list;
+    // Print the list in order
+    printf("ORDERED uhhhhh... ORDER:\n");
+    while (ptr != NULL) {
+        // Point the next node backwards
+        next = ptr->next_node;
+
+    }
+
+return 0;
+
 }
