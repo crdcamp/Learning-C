8da0a25b93cfd0a945452185e90072d798792f2f
Christian Camp <crdcamp@gmail.com>
2026-09-24 18:41:43 -0400

    Linked list initiated

--- practice/1_linked_lists/neet_code_linked_lists.c
+++ practice/1_linked_lists/neet_code_linked_lists.c
@@ -2,32 +2,50 @@
 #include <stdlib.h>
 
 // https://www.youtube.com/watch?v=G0_I-ZF0S38
-// Reverse a singly linked list
+// Reverse a singly linked linked_list
 // Input: 1->2->3->4->5->NULL
 // Output: 5->4->3->2->1->NULL
-// A linked list can be revered either iteratively or recursively
+// A linked linked_list can be revered either iteratively or recursively
 
 typedef struct node {
     int integer;
     struct node *next_pointer;
 } node;
 
-void create_linked_list(node first_pointer);
-void iterative(node first_pointer);
-void recursive(node first_pointer);
+node create_linked_linked_list(node first_pointer);
+void print_linked_linked_list(node linked_list);
+node iterative(node first_pointer);
+node recursive(node first_pointer);
 
 int main(void) {
     return 0;
 }
 
-// Create a linked list of 5 integers
-void create_linked_list(node first_pointer) {
-    for (int i = 0; i < 5; i++) {
+// Create a linked linked_list of 5 integers
+node create_linked_linked_list(node first_pointer) {
+    // Initiate the linked_list
+    node *linked_list = NULL;
 
+    // Iterate though only 5 integers
+    for (int i = 0; i < 5; i++) {
+        // Allocate memory for a node
+        node *n = malloc(sizeof(node));
+        // Assign an integer to the node
+        n->integer = i + 1;
+        // Assign a location for the next pointer
+        n->next_pointer = linked_list;
+        // Update `linked_list''s location for the next iteration
+        linked_list = n;
     }
+
+    return *linked_list;
 }
 
-void iterative(node first_pointer) {
+void print_linked_linked_list(node linked_list) {
+    return;
+}
+
+node iterative(node first_pointer) {
     // So, essentially what we want to do here is reverse
     // the pointers
 
