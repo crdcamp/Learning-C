6c565ddbd8125e5f3f91d523db1bcf77cfd0df68
Christian Camp <crdcamp@gmail.com>
2026-09-25 15:59:49 -0400

    Initial setup

---
+++ practice/1_linked_lists/w3resource_practice/6_insert_middle.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Write a program in C to insert a node in the middle of a Singly Linked List.
+
+typedef struct node {
+    int integer;
+    struct node *next_node;
+} node;
+
+node *create_list(int length);
+void print_list(char *message, node *list);
+node *sort_list(node *list);
+node *insert_middle(node *list);
+
+int main(void) {
+    return 0;
+}
