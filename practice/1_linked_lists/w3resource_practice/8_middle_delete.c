3e5945da558ce7d2c2489b224d8d3d3bb31636ed
Christian Camp <crdcamp@gmail.com>
2026-09-25 20:04:49 -0400

    Function prototypes

--- practice/1_linked_lists/w3resource_practice/8_middle_delete.c
+++ practice/1_linked_lists/w3resource_practice/8_middle_delete.c
@@ -6,6 +6,11 @@ typedef struct node {
     struct node *next_node;
 } node;
 
+void create_list(int length);
+node *list_length(node *list);
+node *list_middle(node *list);
+node *delete_middle(node *list);
+
 // Write a program in C to delete a node from the middle of a Singly Linked List.
 int main(void) {
 
